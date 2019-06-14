defd218e08f480d7b2282b829e1e56b65875ade9
Vikas Mahato <vikasmahato0@gmail.com>
2019-06-14 23:04:56 +0530

    Initial Commit

---
+++ src/main/java/com/project/bms/BmsApplication.java
@@ -0,0 +1,27 @@
+package com.project.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = { 
+		BmsApplication.class,
+		Jsr310JpaConverters.class 
+})
+public class BmsApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/bms/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.project.bms.repository;
+
+import com.project.bms.model.Role;
+import com.project.bms.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
